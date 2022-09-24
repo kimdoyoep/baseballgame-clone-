9cb955b3312394bd82897f929be5ed952377bc1c
kimdoyoep <dpdp39000@gmail.com>
2022-09-25 02:18:45 +0900

    IF :: example

---
+++ Study_Java/src/day02/If.java
@@ -0,0 +1,23 @@
+package day02;
+
+public class If {
+    public static void main(String[] arg){
+
+        int a = 9;
+        if(a >= 8){
+            System.out.println("학교에 가야합니다.");
+        }
+        else{
+            System.out.println("아직 학교에 갈 나이가 아닙니다.");
+        }
+
+
+
+
+
+
+    }
+
+
+    }
+  }
