c29ce2b4166a98af409e226b172c5631d786607f
kimdoyoep <dpdp39000@gmail.com>
2022-09-26 17:49:04 +0900

    Test :: uncomplish

---
+++ Study_Java/src/day03/Test03.java
@@ -0,0 +1,11 @@
+package day03;
+
+public class Test03 {
+    public static void main(String[] args) {
+
+        for(int i = 1; i < 6; i++){
+            char star = '*';
+            System.out.println(star*i);
+        }
+    }
+}
