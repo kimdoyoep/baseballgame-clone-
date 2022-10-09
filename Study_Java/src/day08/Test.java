9521eca99e02f41cc1bbf9c5a8f3ac8416b8cd40
Kimdoyeop <dpdp39000@gmail.com>
2022-10-09 23:55:16 +0900

    study :: Test

--- Study_Java/src/day08/Test.java
+++ Study_Java/src/day08/Test.java
@@ -2,5 +2,17 @@ package day08;
 
 public class Test {
 
+    int num;
+    void aaa(){
+        System.out.println("aaa()출력");
+
+    }
+
+    public static void main(String[] args) {
+        Test a = new Test();
+        a.aaa();
+        Test b = new Test();
+        b.aaa();
+    }
 
 }
