caf3ee4596d7713038adf6669a3c445f6e1024b4
Kimdoyeop <dpdp39000@gmail.com>
2022-10-09 22:12:16 +0900

    study :: intherit

--- Study_Java/src/day07/VIP.java
+++ Study_Java/src/day07/VIP.java
@@ -5,6 +5,12 @@ public class VIP extends Customer { //Customer클래스 상속받음. 겹치는 
     private int agentID; //VIP상담원 아이디
     double saleRatio;//할인율
 
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio; //보너스 포인트 적립
+        return price - (int)(price * saleRatio);  //할인된 가격을 계산하여 반환
+    }
+
     public VIP(){
         customerGrade = "VIP";
         bonusRatio = 0.05;
