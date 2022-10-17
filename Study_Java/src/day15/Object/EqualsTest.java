d1efe68723610978e927534fe6f5e1b470c3dc89
Kimdoyeop <dpdp39000@gmail.com>
2022-10-18 00:13:19 +0900

    Object 나머지..

--- Study_Java/src/day15/Object/EqualsTest.java
+++ Study_Java/src/day15/Object/EqualsTest.java
@@ -9,10 +9,25 @@ public class EqualsTest {
         Equals studentSang = new Equals(100, "이상원");
 
         if(studentLee == studentLee2)
-            System.out.println("studentLee 와 studentLee2는 같습니다.");
+            System.out.println("studentLee 와 studentLee2는 물리적으로 같습니다.");
 
         else
-            System.out.println("studentLee 와 studentLee2는 다릅니다.");
+            System.out.println("studentLee 와 studentLee2는 물리적으로 다릅니다.");
+
+        if(studentLee != studentSang)
+            System.out.println("studentLee 와 studentSang은 물리적으로 다릅니다.");
+        else
+            System.out.println("studentLee 와 studentSang은 물리적으로 다를 수 밖에 없습니다.");
+
+        if(studentLee2 != studentSang)
+            System.out.println("studentLee2 와 studentSang은 물리적으로 다릅니다.");
+        else
+            System.out.println("studentLee2 와 studentSang은 물리적으로 다를 수 밖에 없습니다.");
+
+        if(studentLee.equals(studentSang))
+            System.out.println("studentLee 와 studentSang은 논리적으로 동일합니다");
+        else
+            System.out.println("studentLee 와 studentSang은 논리적으로 다릅니다.");
 
 
 
