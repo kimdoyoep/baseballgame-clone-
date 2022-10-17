5e1bebcafdc683d14e34d1b2c8672750e644f76f
Kimdoyeop <dpdp39000@gmail.com>
2022-10-17 23:40:36 +0900

    Study :: Object

---
+++ Study_Java/src/day15/Object/ToString.java
@@ -0,0 +1,10 @@
+package day15.Object;
+
+public class ToString {
+    public static void main(String[] args) {
+        Book book1 = new Book(200, "개미");
+
+        System.out.println(book1);
+        System.out.println(book1.toString());
+    }
+}
