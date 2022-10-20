81af692ff668be7aa4a3048fce06a2fe9588bc9f
Kimdoyeop <dpdp39000@gmail.com>
2022-10-20 13:09:42 +0900

    Study :: Class-Class.forName

---
+++ Study_Java/src/day18/ClassforName.java
@@ -0,0 +1,13 @@
+package day18;
+
+public class ClassforName {
+
+    /* Class.forName("classex.Person");
+
+    ===== 변수로 Class.forName() 사욯하는 방법 =====
+
+       String className = "classex.Person"
+       Class.forName("className");
+     */
+
+}
