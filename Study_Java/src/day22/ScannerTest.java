7d1c926cc30bc7cec68f7e20baf42325edc245e9
Kimdoyeop <dpdp39000@gmail.com>
2022-10-29 15:49:35 +0900

    Study :: Scanner

---
+++ Study_Java/src/day22/ScannerTest.java
@@ -0,0 +1,20 @@
+package day22;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("이름 : "); //문자열을 읽는 nextLine 메서드로 이름과 직업 입력받음
+        String name = scanner.nextLine();
+        System.out.println("직업 : ");
+        String job = scanner.nextLine();
+        System.out.println("사변 : "); //int 형을 읽는 nextInt 메서드로 사변을 입력받음
+        int num = scanner.nextInt();
+
+        System.out.println(name);
+        System.out.println(job);
+        System.out.println(num);
+    }
+}
