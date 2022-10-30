6124472326e111ddefe4efe84088df7c17694925
Kimdoyeop <dpdp39000@gmail.com>
2022-10-30 23:38:37 +0900

    Test :: SelfIntroduction

---
+++ Study_Java/src/CloudStudingJava1000/Test02.java
@@ -0,0 +1,9 @@
+package CloudStudingJava1000;
+
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요 반갑습니다.");
+        System.out.println("저는 열혈 청년 홍길동 입니다.");
+        System.out.println("잘 부탁드리겠습니다!");
+    }
+}
