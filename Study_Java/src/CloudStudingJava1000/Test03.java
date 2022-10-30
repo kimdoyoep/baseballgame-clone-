1ce1f97f2fd05ed8fc26b0a744d41bd94bd9f7db
Kimdoyeop <dpdp39000@gmail.com>
2022-10-30 23:47:04 +0900

    Test :: Profile

---
+++ Study_Java/src/CloudStudingJava1000/Test03.java
@@ -0,0 +1,18 @@
+package CloudStudingJava1000;
+
+public class Test03 {
+    public static void main(String[] args) {
+
+        int StudentNumber = 2601;
+        double StudentTall = 181;
+        String StudentName = "김도엽";
+        boolean isMale = true;
+
+        System.out.println("이름 : " + StudentName);
+        System.out.println("키 : " + StudentTall );
+        System.out.println("학번 : " + StudentNumber);
+        System.out.println("남자인가요? : " + isMale);
+
+
+    }
+}
