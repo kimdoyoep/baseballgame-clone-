ba63e721d03310751f4464618505ada1c3b67a7b
Kimdoyeop <dpdp39000@gmail.com>
2022-10-30 23:58:41 +0900

    Test :: Food

---
+++ Study_Java/src/CloudStudingJava1000/Test04.java
@@ -0,0 +1,7 @@
+package CloudStudingJava1000;
+
+public class Test04 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Study_Java/src/CloudStudingJava1000/Test05/Fork.java
@@ -0,0 +1,8 @@
+package CloudStudingJava1000.Test05;
+
+public class Fork {
+    String ForkName;
+    int money;
+    int ForkCount;
+
+}
