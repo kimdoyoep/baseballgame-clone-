024831000757d42e6473b08f0db363b97bcd4ded
Kimdoyeop <dpdp39000@gmail.com>
2022-10-31 22:53:39 +0900

    Test :: Alba

---
+++ Study_Java/src/CloudStudingJava1000/Test06/Alba.java
@@ -0,0 +1,22 @@
+package CloudStudingJava1000.Test06;
+
+import java.util.Scanner;
+
+public class Alba {
+    public static void main(String[] args) {
+
+        int Price;
+        int time;
+
+        Scanner sr = new Scanner(System.in);
+
+        System.out.println("시급을 입력하세요");
+        Price = sr.nextInt();
+
+        System.out.println("일한 시간을 입력하세요");
+        time = sr.nextInt();
+
+        System.out.println(Price*time);
+
+    }
+}
