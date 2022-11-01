2156651ee256f65251891a1af661b532985b0d80
Kimdoyeop <dpdp39000@gmail.com>
2022-11-01 22:31:36 +0900

    Study :: String

---
+++ Study_Java/src/day23/StringApp.java
@@ -0,0 +1,15 @@
+package day23;
+
+public class StringApp {
+    public static void main(String[] args) {
+
+        // new line (줄바꿈)
+        System.out.println("Hello \nWorld");
+        // escape
+        System.out.println("Hello \"World\""); // Hello "World"
+        // replace
+        System.out.println("안녕하세요. 잘 지내시죠? [[[Name]]]씨?".replace("[[[Name]]]","홍길동"));
+
+
+    }
+}
